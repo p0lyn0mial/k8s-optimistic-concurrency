d0103e2c403af15dc35a76bedaa2deaa19293538
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2024-08-02 08:23:28 +0200

    initial commit

---
+++ controller_one.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	corev1informers "k8s.io/client-go/informers/core/v1"
+	corev1client "k8s.io/client-go/kubernetes/typed/core/v1"
+	corev1listers "k8s.io/client-go/listers/core/v1"
+
+	"github.com/openshift/library-go/pkg/controller/factory"
+	"github.com/openshift/library-go/pkg/operator/events"
+)
+
+type controllerOne struct {
+	updateCounter   int
+	client          corev1client.CoreV1Interface
+	configMapLister corev1listers.ConfigMapLister
+}
+
+func newControllerOne(
+	client corev1client.CoreV1Interface,
+	coreInformers corev1informers.Interface,
+	recorder events.Recorder,
+) factory.Controller {
+	c := &controllerOne{
+		client:          client,
+		configMapLister: coreInformers.ConfigMaps().Lister(),
+	}
+
+	return factory.New().
+		WithInformers(coreInformers.ConfigMaps().Informer()).
+		WithSync(c.sync).ResyncEvery(10*time.Second).
+		ToController("controllerOne", recorder.WithComponentSuffix("controller-one"))
+}
+
+func (c *controllerOne) sync(ctx context.Context, _ factory.SyncContext) error {
+	configMap, err := c.configMapLister.ConfigMaps("foo").Get("bar")
+	if err != nil {
+		return err
+	}
+	configMapCopy := configMap.DeepCopy()
+	configMapCopy.Data["controllerOne"] = fmt.Sprintf("alive-%d", c.updateCounter)
+
+	_, err = c.client.ConfigMaps("foo").Update(ctx, configMapCopy, metav1.UpdateOptions{})
+	c.updateCounter++
+	return err
+}
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"github.com/openshift/library-go/pkg/operator/events"
+	"time"
+
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/klog/v2"
+
+	configv1 "github.com/openshift/api/config/v1"
+	"github.com/openshift/library-go/pkg/config/helpers"
+)
+
+func main() {
+	var kubeConfig string
+	klog.InitFlags(flag.CommandLine)
+	flag.StringVar(&kubeConfig, "kubeconfig", "", "")
+	flag.Parse()
+
+	klog.Info("starting the controllers")
+	config, err := helpers.GetKubeConfigOrInClusterConfig(kubeConfig, configv1.ClientConnectionOverrides{})
+	if err != nil {
+		panic(err)
+	}
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+	kubeInformers := informers.NewSharedInformerFactory(client, 1*time.Hour)
+	memoryRecorder := events.NewInMemoryRecorder("k8s-oc")
+
+	ctrlOne := newControllerOne(client.CoreV1(), kubeInformers.Core().V1(), memoryRecorder)
+	ctrlTwo := newControllerTwo(client.CoreV1(), kubeInformers.Core().V1(), memoryRecorder)
+
+	ctx := setupSignalContext(context.Background())
+	kubeInformers.Start(ctx.Done())
+	go ctrlOne.Run(ctx, 1)
+	go ctrlTwo.Run(ctx, 1)
+
+	klog.Info("Waiting for SIGTERM or SIGINT signal to initiate shutdown")
+	<-ctx.Done()
+}
